a25a6243353d179a41c6a4476c948280a6d37357
Orbit235 <v.ionichev@gmail.com>
2022-12-24 07:28:27 +0300

    app created

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  priceForm = this.fb.group({
+    name: ['', Validators.required],
+    phone: ['', Validators.required],
+    cars: ['', Validators.required]
+
+  })
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  goScroll(target: HTMLElement) {
+    target.scrollIntoView({behavior: "smooth"});
+  }
+
+  onSubmit() {
+    if (this.priceForm.valid) {
+      alert("Спасибо за заявку, мы свяжемся с вами в ближайшее время");
+      this.priceForm.reset()
+    }
+  }
+}
